6b0a6252833e6d5bdf35d35595b6e731b07bdfe5
Tommy Sarni <tsarn1799@gmail.com>
2024-09-06 15:20:19 -0400

    prop component finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import './App.css'
-import EffectHookComponent from './challenges/EffectHookComponent';
-import StateHookComponent from './challenges/StateHookComponent';
-import PracticeItem from './components/PracticeItem';
+import "./App.css";
+import EffectHookComponent from "./challenges/EffectHookComponent";
+import PropsComponent from "./challenges/Props";
+import StateHookComponent from "./challenges/StateHookComponent";
+import PracticeItem from "./components/PracticeItem";
 
 function App() {
-
-
   return (
     <>
       <h1>React Practices</h1>
-      <div className='maincontent'>
-        <PracticeItem title={"useState Hook Example"} component={ <StateHookComponent />} description={'Boss Battle Component with enemy health state.'} />
-        <PracticeItem title={"useEffect Hook Example"} component={ <EffectHookComponent />} description={'Easing Function Visualizers'} />
+      <div className="maincontent">
+        <PracticeItem
+          title={"useState Hook Example"}
+          component={<StateHookComponent />}
+          description={"Boss Battle Component with enemy health state."}
+        />
+        <PracticeItem
+          title={"useEffect Hook Example"}
+          component={<EffectHookComponent />}
+          description={"Easing Function Visualizers"}
+        />
+        <PracticeItem
+          title={"Child-Parent Prop Example with State"}
+          component={<PropsComponent />}
+          description={"Dashboard with 2 Switch Components"}
+        />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/challenges/Props/index.jsx
+++ src/challenges/Props/index.jsx
@@ -1,11 +1,75 @@
-// Component to practice uni-directionality (aka pass down props)
+import { useState } from "react";
+import "./index.css";
 
+/**
+ * Dashboard with 2 switches with parent state
+ * @returns Switch Dashboard
+ */
 const PropsComponent = () => {
-    return (
-        <div>
-            <h3>Props Component Goes Here</h3>
-        </div>
-    )
-}
+  return <Dashboard />;
+};
 
-export default PropsComponent
+const Dashboard = () => {
+  const [s1, setS1] = useState(true);
+  const [s2, setS2] = useState(true);
+
+  const toggleS1 = () => {
+    setS1((prev) => !prev);
+  };
+  const toggleS2 = () => {
+    setS2((prev) => !prev);
+  };
+
+  return (
+    <div className="dashboardContainer">
+      <h3>Initial State</h3>
+      <div style={{ display: "flex", gap: "40px" }}>
+        <div>
+          <input type="checkbox" onChange={() => toggleS1()} checked={s1} />
+        </div>
+        <div>
+          <input type="checkbox" onChange={() => toggleS2()} checked={s2} />
+        </div>
+      </div>
+
+      <Switch on={s1} setOn={toggleS1} id={"s1"} />
+      <Switch on={s2} setOn={toggleS2} id={"s2"} />
+    </div>
+  );
+};
+
+/**
+ * Toggle Switch Component
+ * @param {object} {on: bool, setOn: () => {}, id: string}
+ */
+//eslint-disable-next-line
+const Switch = ({ on, setOn, id }) => {
+  const clickSwitch = (e) => {
+    const btn = e.target;
+
+    if (btn.style.animationPlayState === "running") {
+      btn.style.animationPlayState = "cancelled";
+      return;
+    }
+    btn.style.animationPlayState = "running";
+
+    btn.onanimationend = () => {
+      btn.classList.toggle("on");
+      setOn();
+      btn.style.animationPlayState = "paused";
+    };
+  };
+
+  return (
+    <div className="toggleContainer">
+      <div className="hole"></div>
+      <button
+        id={id}
+        onClick={(e) => clickSwitch(e)}
+        className={`switchContainer element ${on ? "on" : ""}`}
+      ></button>
+    </div>
+  );
+};
+
+export default PropsComponent;
